6ea6d2a71f52fefe661ff8b90027b449899f8744
Stefan <stefan@stefanpuia.co.uk>
2022-06-10 19:47:14 +0100

    replaced unpkg netlifycms js

--- .eleventy.js
+++ .eleventy.js
@@ -6,16 +6,12 @@ const markdownPaired = require("./eleventy/shortcode/paired/markdown");
 module.exports = (config) => {
   passthrough.forEach((pt) => config.addPassthroughCopy(pt));
   [...dateFilters].forEach(([name, fn]) => config.addFilter(name, fn));
-  [...nodeEnv].forEach(([name, fn]) =>
-    config.addShortcode(name, fn)
-  );
-  [...markdownPaired].forEach(([name, fn]) =>
-    config.addPairedShortcode(name, fn)
-  );
+  [...nodeEnv].forEach(([name, fn]) => config.addShortcode(name, fn));
+  [...markdownPaired].forEach(([name, fn]) => config.addPairedShortcode(name, fn));
   config.setBrowserSyncConfig({
     snippetOptions: {
-      ignorePaths: "/admin/**"
-    }
+      ignorePaths: "/admin/**",
+    },
   });
 
   return {
